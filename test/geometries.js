adf9c9c62b414211d8e05b0cccd99fbaea7cc959
Cid Donham <cdubs8600@gmail.com>
2015-04-30 18:08:05 -0700

    unit test passing

--- test/geometries.js
+++ test/geometries.js
@@ -2,13 +2,14 @@ var test = require('blue-tape');
 var geometries = require('../').geometries;
 
 test('can create a circle GeoJSOn from geometries.circle', function (t) {
+  var shape = 'Circle';
   var center = [1.0, 2.0];
   var radius = 5.0;
-  var units = 'km' || 'mi';
-  var geometry = geometries.circle(center, radius, units);
-  t.equal(geometry.type, 'Circle');
+  var units = 'mi' || 'km';
+  var geometry = geometries.circle(shape, center, radius, units);
+  //t.equal(geometry.type, 'Circle');
   t.deepEqual(geometry, {
-    type: "Circle",
+    type: shape,
     coordinates: center,
      radius: radius,
      properties: {
@@ -17,17 +18,3 @@ test('can create a circle GeoJSOn from geometries.circle', function (t) {
   });
   t.end();
 });
-
-
-/*
-
-{
-    "type": "Circle",
-    "coordinates": [ geoCoords.x, geoCoords.y ],
-     "radius": r,
-     "properties": {
-        "radius_units": "mi"
-      }
-  };
-
-  */
