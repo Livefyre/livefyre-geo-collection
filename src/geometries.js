82eb7afa5e1cc89a0ebe0ce748fc2b7b0c92f835
Cid Donham <cdubs8600@gmail.com>
2015-04-30 18:39:38 -0700

    circle test passing

--- src/geometries.js
+++ src/geometries.js
@@ -1,12 +1,29 @@
-exports.circle = function (shape, center, radius, unit) {
-  console.log('making a circle', arguments)
-  var geometry = {
-    "type": shape,
-    "coordinates": [center[0], center[1]],
-     "radius": radius,
-     "properties": {
-        "radius_units": unit
-      }
+'use strict'
+exports.circle = function (type, center, radius, units) {
+  console.log('making a circle', arguments);
+
+  var geometryCircle = {
+    type: type,
+    coordinates: [center[0], center[1]],
+    radius: radius,
+    properties: {
+      radius_units: units
+    }
   };
-  return geometry;
-}
+
+  return geometryCircle;
+};
+
+// exports.polygon = function (type, coordinates, props) {
+//   console.log('making a polygon', arguments)
+//   var geometry = {
+//     "type": type,
+//     "coordinates": coordinates
+//     },
+//     "properties": {
+//       "prop0": props[0],
+//       "prop1": props[1]
+//     };
+
+//   return geometry;
+// }
--- test/geometries.js
+++ test/geometries.js
@@ -2,14 +2,14 @@ var test = require('blue-tape');
 var geometries = require('../').geometries;
 
 test('can create a circle GeoJSOn from geometries.circle', function (t) {
-  var shape = 'Circle';
+  var type = 'Circle';
   var center = [1.0, 2.0];
   var radius = 5.0;
   var units = 'mi' || 'km';
-  var geometry = geometries.circle(shape, center, radius, units);
+  var geometry = geometries.circle(type, center, radius, units);
   //t.equal(geometry.type, 'Circle');
   t.deepEqual(geometry, {
-    type: shape,
+    type: type,
     coordinates: center,
      radius: radius,
      properties: {
@@ -18,3 +18,42 @@ test('can create a circle GeoJSOn from geometries.circle', function (t) {
   });
   t.end();
 });
+
+// test('can create a polygon GeoJSOn from geometries.polygon', function (t) {
+//   var type = 'Polygon';
+//   var coordinates = [[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]];
+//   var props = ['value0', {'this': 'that'}];
+//   var geometry = geometries.polygon(type, coordinates, props);
+//   //t.equal(geometry.type, 'Circle');
+//   t.deepEqual(geometry,
+//     {
+//       type: type,
+//       coordinates: coordinates
+//     },
+//       properties: {
+//         prop0: props[0],
+//         prop1: props[1]
+//     }
+//   });
+//   t.end();
+// });
+
+
+/*
+
+{
+    "type": "Circle",
+    "coordinates": [ geoCoords.x, geoCoords.y ],
+     "radius": r,
+     "properties": {
+        "radius_units": "mi"
+      }
+  };
+
+  {
+     "type": "Polygon",
+     "coordinates":
+       ]
+    }
+
+  */
