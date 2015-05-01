0bc906479a2f31a994acb38a4155d25ff249344f
Cid Donham <cdubs8600@gmail.com>
2015-05-01 12:27:04 -0700

    polygon passing tests

--- test/geometries.js
+++ test/geometries.js
@@ -6,9 +6,10 @@ test('can create a circle GeoJSOn from geometries.circle', function (t) {
   var center = [1.0, 2.0];
   var radius = 5.0;
   var units = 'mi' || 'km';
-  var geometry = geometries.circle(type, center, radius, units);
+
+  var geometryCircle = geometries.circle(type, center, radius, units);
   //t.equal(geometry.type, 'Circle');
-  t.deepEqual(geometry, {
+  t.deepEqual(geometryCircle, {
     type: type,
     coordinates: center,
      radius: radius,
@@ -19,41 +20,23 @@ test('can create a circle GeoJSOn from geometries.circle', function (t) {
   t.end();
 });
 
-// test('can create a polygon GeoJSOn from geometries.polygon', function (t) {
-//   var type = 'Polygon';
-//   var coordinates = [[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]];
-//   var props = ['value0', {'this': 'that'}];
-//   var geometry = geometries.polygon(type, coordinates, props);
-//   //t.equal(geometry.type, 'Circle');
-//   t.deepEqual(geometry,
-//     {
-//       type: type,
-//       coordinates: coordinates
-//     },
-//       properties: {
-//         prop0: props[0],
-//         prop1: props[1]
-//     }
-//   });
-//   t.end();
-// });
-
-
-/*
-
-{
-    "type": "Circle",
-    "coordinates": [ geoCoords.x, geoCoords.y ],
-     "radius": r,
-     "properties": {
-        "radius_units": "mi"
-      }
+test('can create a polygon GeoJSOn from geometries.polygon', function (t) {
+  var type = 'Polygon';
+  var coordinates = [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ];
+  var props = {
+    "prop0": "value0",
+    "prop1": {
+      "this": "that"
+    }
   };
 
-  {
-     "type": "Polygon",
-     "coordinates":
-       ]
-    }
+  var geometry = geometries.polygon(type, coordinates, props);
+  //t.equal(geometry.type, 'Polygon');
 
-  */
+  t.deepEqual(geometry, {
+    type: type,
+    coordinates: coordinates,
+    properties: props
+  });
+  t.end();
+});
