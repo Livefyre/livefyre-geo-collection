0eae623be8397f8c543949489d9ff643cd75b68e
Benjamin Goering <bengoering@gmail.com>
2015-05-04 17:14:14 -0700

    tile-archive does until paging

--- src/tile-archive.js
+++ src/tile-archive.js
@@ -8,13 +8,13 @@ module.exports = function createTileArchive(opts) {
   archive._read = function () {
     var hasPrev = paging && paging.hasPrev;
     if (paging) {
-      if (hasPrev) {
-        // console.error(new Error('There is previous content but I dont yet know how to get it from the API! Ending for now #TODO'))
+      if ( ! hasPrev) {
+        // all done
         return archive.push(null);
-      } else {
-        // we done
-        return archive.push(null);        
       }
+      // add paging opts from last paging response
+      opts = Object.create(opts);
+      opts.until = Math.ceil(paging.prev);
     }
 
     fetchGeo(opts)
