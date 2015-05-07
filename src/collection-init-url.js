5a68ef58e93c72375f5cec13007115a18917a009
Benjamin Goering <bengoering@gmail.com>
2015-05-07 12:21:19 -0700

    Add collection-init-url and factor out common things to livefyre-url

--- src/collection-init-url.js
+++ src/collection-init-url.js
@@ -3,30 +3,20 @@ var base64 = require('./base64');
 var livefyreUrl = require('./livefyre-url');
 var querystring = require('querystring');
 
-var geoUrlTemplate = '{protocol}//{host}/bs3/v3.1/{network}/{siteId}/{b64urlArticleId}/geojson/{z}/{x}/{y}.json';
+var initUrlTemplate = '{protocol}//{host}/bs3/v3.1/{network}/{siteId}/{b64urlArticleId}/init';
 /**
- * Get the URL of geo-indexed content of a Livefyre Collection
+ * Get the URL of the init resource of a Livefyre Collection
  * @param collection {object} - { network, siteId, articleId }
- * @param tile.x {number} - x coordinate of map tile
- * @param tile.y {number} - y coordinate of map tile
- * @param tile.z {number} - z coordinate of map tile
  * @param [protocol='https:'] - protocol to use in URL
  */
 module.exports = function (opts) {
   var collection = opts.collection;
-  var tile = opts.tile;
-  var url = livefyreUrl.renderURLTemplate(geoUrlTemplate, {
+  var url = livefyreUrl.renderURLTemplate(initUrlTemplate, {
     host: livefyreUrl.apiHostForNetwork(collection.network, 'bootstrap'),
     protocol: opts.protocol || 'https:',
     network: collection.network,
     siteId: collection.siteId,
     b64urlArticleId: base64.pad(base64.urlEncode(collection.articleId)),
-    x: tile.x,
-    y: tile.y,
-    z: tile.z
   });
-  if (opts.until) {
-    url = [url, querystring.stringify({ until: opts.until })].join('?')
-  }
   return url;
 }
